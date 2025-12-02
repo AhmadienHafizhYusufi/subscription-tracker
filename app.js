ee3730ad87147b6babb84abe4e9fd590a0e38bbb
AHMADIEN HAFIZH YUSUFI <ahmadienhafizhyusufi@gmail.com>
2025-12-03 06:59:21 +0700

    Error Handling

--- app.js
+++ app.js
@@ -6,12 +6,20 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = e();
+
+app.use(e.json());
+app.use(e.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the subscription Tracker API!");
@@ -22,7 +30,7 @@ app.listen(PORT, async () => {
     `Subscription Tracker API is running on http://localhost:${PORT}`
   );
 
-  await connectToDatabase()
+  await connectToDatabase();
 });
 
 export default app;
