f89d4557e53421d26b03a0561bd2aa14d1513c52
AHMADIEN HAFIZH YUSUFI <ahmadienhafizhyusufi@gmail.com>
2025-12-02 13:55:42 +0700

    API Routes

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ import e from "express";
 
 import { PORT } from "./config/env.js";
 
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+
 const app = e();
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the subscription Tracker API!");
